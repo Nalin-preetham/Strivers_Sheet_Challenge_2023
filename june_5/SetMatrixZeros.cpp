b659662969f3b3dd184b3ece9190c6c76b8f8627
Nalin-preetham <126316191+Nalin-preetham@users.noreply.github.com>
2023-06-05 11:37:19 +0530

    Update SetMatrixZeros.cpp

--- june_5/SetMatrixZeros.cpp
+++ june_5/SetMatrixZeros.cpp
@@ -1,1 +1,40 @@
+#include <bits/stdc++.h>
+ void setZero(vector<vector<int>> &matrix,int row,int col,int n,int m)
+ {
+     for(int i=0;i<n;i++)
+     {
+         matrix[i][col]=0;
+     }
+     for(int i=0;i<m;i++)
+     {
 
+         matrix[row][i]=0;
+     }
+ }
+void setZeros(vector<vector<int>> &matrix)
+{
+	
+    int n=matrix.size();
+    int m=matrix[0].size();
+    vector<vector<int>> temp(n,vector<int>(m));
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            temp[i][j]=matrix[i][j];
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+
+        {
+            if(temp[i][j]==0)
+            {
+                setZero(matrix,i,j,n,m);
+            }
+        }
+    }
+
+
+}
