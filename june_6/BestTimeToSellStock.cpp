a057e46cb40bf2a7eb473c6bad14f15abb4eb894
Nalin-preetham <126316191+Nalin-preetham@users.noreply.github.com>
2023-06-06 17:37:40 +0530

    Create BestTimeToSellStock.cpp

---
+++ june_6/BestTimeToSellStock.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h> 
+int maximumProfit(vector<int> &prices){
+    int profit=0;
+    int mini=INT_MAX;
+    for(int i=0;i<prices.size();i++)
+    {
+        mini=min(mini,prices[i]);
+       profit=max(profit,prices[i]-mini);
+ }
+    return profit;
+}
