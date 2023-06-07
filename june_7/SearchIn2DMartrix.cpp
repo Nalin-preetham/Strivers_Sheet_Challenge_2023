5e691d95bb591586f7b04fb51eec0019daf71469
Nalin-preetham <126316191+Nalin-preetham@users.noreply.github.com>
2023-06-07 11:56:08 +0530

    Create SearchIn2DMartrix.cpp

---
+++ june_7/SearchIn2DMartrix.cpp
@@ -0,0 +1,18 @@
+bool searchMatrix(vector<vector<int>>& mat, int target) {
+    int n=mat.size();
+    int m=mat[0].size();
+ int low=0;
+ int high=n*m-1;  
+ while(low<=high)
+ {
+     int mid=(low+high)/2;
+     if(mat[mid/m][mid%m]==target)
+     return true;
+     else if(mat[mid/m][mid%m]<target)
+     low=mid+1;
+     else
+     high=mid-1;
+     
+ }
+ return false;       
+}
