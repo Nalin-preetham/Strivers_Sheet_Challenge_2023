557734ee8493d077d86e4b0d4aef68a4e3654bf4
Nalin-preetham <126316191+Nalin-preetham@users.noreply.github.com>
2023-06-08 19:06:29 +0530

    Create UniquePaths.cpp

---
+++ june_8/UniquePaths.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+int solve(int m,int n,vector<vector<int>> &dp)
+{
+	if(m==0 && n==0)
+	return 1;
+	if(m<0 || n<0)
+	return 0;
+	if(dp[m][n]!=-1)
+	return dp[m][n];
+	int left=solve(m-1,n,dp);
+	int right=solve(m,n-1,dp);
+	return dp[m][n]=left+right;
+}
+int uniquePaths(int m, int n)  
+{
+	vector<vector<int>> dp(m,vector<int>(n,-1));
+	return solve(m-1,n-1,dp);
+}
